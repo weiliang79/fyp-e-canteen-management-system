f72ad826766726120fd938e28a5b3dad2bfafe14
Wei Liang <liang2010@hotmail.my>
2022-10-28 14:21:59 +0800

    feat: home page for admin and food seller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\OrderDetail;
+use Auth;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +27,54 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(Auth::user()->isAdmin()){
+
+            $startDate = Carbon::today()->firstOfMonth();
+            $endDate = $startDate->copy();
+            $endDate->endOfMonth();
+
+            $orderCount = Order::whereBetween('created_at', [$startDate, $endDate])
+                ->where('status', Order::PICKUP_ALL)
+                ->count();
+
+            $allOrderCount = Order::whereBetween('created_at', [$startDate, $endDate])
+                ->count();
+
+            $sales = Order::whereBetween('created_at', [$startDate, $endDate])
+            ->where('status', Order::PAYMENT_SUCCESS)
+            ->sum('total_price');
+
+            return view('admin.home', compact('orderCount', 'allOrderCount', 'sales'));
+            
+        } else if (Auth::user()->isFoodSeller()) {
+
+            $startDate = Carbon::today()->firstOfMonth();
+            $endDate = $startDate->copy();
+            $endDate->endOfMonth();
+
+            $startTime = Carbon::today()->startOfDay();
+            $endTime = $startTime->copy();
+            $endTime->endOfDay();
+
+            $orderCount = OrderDetail::whereHas('order', function ($query) use ($startTime, $endTime) {
+                $query->whereBetween('pick_up_start', [$startTime, $endTime]);
+            })->where('is_pickup', true)->count();
+
+            $allOrderCount = OrderDetail::whereHas('order', function ($query) use ($startTime, $endTime) {
+                $query->whereBetween('pick_up_start', [$startTime, $endTime]);
+            })->count();
+
+            $sales = OrderDetail::whereHas('order', function ($query) use ($startDate, $endDate) {
+                $query->where('status', Order::PAYMENT_SUCCESS)
+                    ->whereBetween('created_at', [$startDate, $endDate]);
+            })->sum('price');
+
+            //dd($startTime, $endTime, $orderCount, $allOrderCount);
+
+            return view('food_seller.home', compact('orderCount', 'allOrderCount', 'sales'));
+        }
+
+        Auth::logout();
+        return redirect()->route('login')->with('swal-warning', 'Unknown error, please contact administrator if it happen again.');
     }
 }
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container py-4">
+      <div class="row justify-content-center">
+            <div class="col-md-8">
+                  <div class="card">
+                        <div class="card-header">
+                              {{ __('Home') }}
+                        </div>
+
+                        <div class="card-body">
+
+                              <div class="row" style="height: 5rem;">
+                                    <div class="col">
+                                          <div class="card bg-primary h-100">
+                                                <div class="d-flex justify-content-between h-100 px-3">
+                                                      <div class="align-self-center">
+                                                            <h6 class="mb-0 text-white">{{ __('Orders in This Month: ') }}</h6>
+                                                            <h5 class="mb-0 text-white">{{ $orderCount . '/' . $allOrderCount }}</h5>
+                                                      </div>
+                                                      <div class="align-self-center">
+                                                            <i class="fa-solid fa-file-invoice-dollar fa-2xl text-white"></i>
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </div>
+
+                                    <div class="col">
+                                          <div class="card bg-success h-100">
+                                                <div class="d-flex justify-content-between h-100 px-3">
+                                                      <div class="align-self-center">
+                                                            <h6 class="mb-0 text-white">{{ __('Sales in This Month: ') }}</h6>
+                                                            <h5 class="mb-0 text-white">{{ config('payment.currency_symbol') . $sales }}</h5>
+                                                      </div>
+                                                      <div class="align-self-center">
+                                                            <i class="fa-solid fa-money-bills fa-2xl text-white"></i>
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
+
+                        </div>
+                  </div>
+            </div>
+      </div>
+</div>
+
+@endsection
